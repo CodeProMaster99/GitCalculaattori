7a3c852d8a40101a466ca4b56339865a8eee5381
Chipolino <xamza.fi@gmail.com>
2022-03-03 13:03:09 +0200

    Lisätty jako sekä kerta ja tehty jokaiselle oma metodi

--- Program.cs
+++ Program.cs
@@ -18,20 +18,28 @@ namespace GitCalculaattori
             Console.WriteLine("Anna toka numero");
             int toka = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Kumpi tulostetaan: summa vai erotus?");
+            Console.WriteLine("Kumpi tulostetaan: summa, erotus, kertaa tai jako?");
             string which = Console.ReadLine();
             Console.WriteLine("\n");
 
 
             if (which == "summa")
             {
-                Console.WriteLine("Summa : " + LaskeSumma(eka, toka));
+                Console.WriteLine("Summa : " + Summa(eka, toka));
                 //vastaus = LaskeSumma(eka,toka);
                 //Console.WriteLine("Summa : " + (eka + toka));
             }
             else if (which == "erotus")
             {
-                Console.WriteLine("Erotus : " + (eka - toka));
+                Console.WriteLine("Erotus : " + Erotus(eka, toka));
+            }
+            else if (which == "kertaa")
+            {
+                Console.WriteLine("Kertaa : " + Kertaa(eka, toka));
+            }
+            else if (which == "jako")
+            {
+                Console.WriteLine("Jako : " + Jako(eka, toka));
             }
             else
             {
@@ -44,10 +52,23 @@ namespace GitCalculaattori
 
         }
 
-        public static int LaskeSumma(int eka, int toka)
+        public static int Summa(int eka, int toka)
         {
             //int summa = eka + toka;
             return eka + toka;
+        }
+        public static int Erotus(int eka, int toka)
+        {
+            return eka - toka;
+        }
+        public static int Kertaa(int eka, int toka)
+        {
+            return eka * toka;
+        }
+        public static double Jako(double eka, double toka)
+        {
+
+            return eka / toka;
         }
 
     }

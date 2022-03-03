679344cb1a6130383f3477bc5123f045c990b904
Chipolino <xamza.fi@gmail.com>
2022-03-03 11:01:03 +0200

    Laskee kahden numeron summan

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace GitCalculaattori
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Anna eka numero");
+            int eka = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Anna toka numero");
+            int toka = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Summa : " + (eka + toka));
+            Thread.Sleep(3000);
+
+
+        }
+    }
+}
